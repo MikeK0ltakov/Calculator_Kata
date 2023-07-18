3571d2b30f9d711f3c0569289a14f159bdcdb100
MikeK0ltakov <138697237+MikeK0ltakov@users.noreply.github.com>
2023-07-18 18:56:08 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -3,77 +3,98 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        String calcf = scn.nextLine();
-        String answer = calculator.calc(calcf);
-        System.out.print(answer);
+        String num = scn.nextLine();
+        String ans = calculator.calc(num);
+        System.out.print("\""+ans+"\"");
     }
 }
-class calculator {
-    public static String calc(String arg) {
-        Roma nmb = new Roma();
-        String[] actions = {"+", "-", "/", "*"};
-        String[] reaction = {"\\+", "-", "/", "\\*"};
+class calculator{
+    public static String calc(String viv){
+        Int y = new Int();
+        String[] actions = {" + ", " - ", " / ", " * "};
+        String[] reaction = {" \\+ ", " - ", " / ", " \\* "};
         int ind = -1;
-        try {
+        boolean start = viv.startsWith("\"");
+        try{
+            if (!start){
+                throw new ArithmeticException("Неправильный ввод");
+        }
             for (int x = 0; x < actions.length; x++)
-                if (arg.contains(actions[x])) {
+                if (viv.contains(actions[x])) {
                     ind = x;
                     break;
                 }
             if (ind == -1) {
-                throw new ArithmeticException("Неправильная операция над числами");
+                throw new ArithmeticException("Неправильная операция");
             }
-        } catch (ArithmeticException e) {
+        }catch (ArithmeticException e){
             System.out.println(e);
+            System.exit(0);
         }
-        String[] data = arg.split(reaction[ind]);
+        String[] data = viv.split(reaction[ind]);
         try{
-            if (data.length > 2){
-                throw new ArithmeticException("Числе должно быть 2");
+            if ((ind == 2 | ind == 3) && data[1].startsWith("\"") && data[0].startsWith("\"")){
+                throw new ArithmeticException("Неправильная операция");
+            }
+            if (!data[1].startsWith("\"") && data[0].startsWith("\"") && (ind == 0 | ind == 1)){
+                throw new ArithmeticException("Неправильная операция");
+            }
+            if (data.length > 2 | data.length <= 1){
+                throw new ArithmeticException("Введены некорректные данные");
+            }
+            int t = y.strInt(data[1].replace("\"", ""));
+            if (t == -1 & !data[1].startsWith("\"")) {
+                throw new ArithmeticException("Числа должны быть целыми и от 1 до 10 включительно");
+            }
+            if (data[0].length()>12 | data[1].length()>12){
+                throw new ArithmeticException("Длина строк до 10");
             }
         }
         catch (ArithmeticException e){
             System.out.print(e);
+            System.exit(0);
         }
-        try {
-            if (nmb.isRoman(data[0]) == nmb.isRoman(data[1])) {
-                int a,b;
-                boolean isRoman = nmb.isRoman(data[0]);
-                try {
-                    if (isRoman) {
-                        a = nmb.RomToInt(data[0]);
-                        b = nmb.RomToInt(data[1]);
-                    } else {
-                        a = Integer.parseInt(data[0]);
-                        b = Integer.parseInt(data[1]);
-                    }
-                    int result = switch (actions[ind]) {
-                        case "+" -> a + b;
-                        case "-" -> a - b;
-                        case "/" -> a / b;
-                        case "*" -> a * b;
-                        default -> 0;};
-                    if (a > 10 | b > 10 | a < 0 | b < 0) {
-                        throw new ArithmeticException("Числа должны быть от 1 до 10 включительно");
-                    }
-                    if (isRoman) {
-                        return nmb.IntToRom(result);
-                    }
-                    else {
-                        return Integer.toString(result);
-                    }
+        data[0] = data[0].replace("\"", "");
+        data[1] = data[1].replace("\"", "");
+        String result = switch(actions[ind]){
+            case " + ":
+                String s = data[0] + data[1];
+                yield s;
+            case " * ":
+                String d = "";
+                for (int i = 0; i< y.strInt(data[1]);i++){
+                    d += data[0];
                 }
-                catch (ArithmeticException e) {
-                    System.out.println(e);
+                yield d;
+            case " - ":
+                String f = "";
+                String repl =data[0].replace(data[1],"");
+                f += repl;
+                yield f;
+            case " / ":
+                String j = "";
+                int l;
+                l = data[0].length() / y.strInt(data[1]);
+                char[] dst = new char[l];
+                data[0].getChars(0,l,dst,0);
+                for (char c : dst) {
+                    j += c;
                 }
-            }
-            else {
-                throw new ArithmeticException("Числа из разных алфавитов");
-            }
+                yield j;
+            default:
+                yield "0";
+        };
+    String newres = "";
+    if (result.length()>40){
+        char[] dst = new char[40];
+        result.getChars(0,40,dst,0);
+        for (char c : dst) {
+            newres += c;
         }
-        catch (ArithmeticException e) {
-            System.out.println(e);
-        }
-        return Integer.toString(Integer.parseInt(arg));
+        newres += "...";
+        return newres;
+    }else {
+        return result;
+    }
     }
 }
